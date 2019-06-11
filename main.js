8a7b1475043061fa0935acf38cbdae28fa0c16da
Seyoung Yoon <sseang011@gmail.com>
2019-06-11 13:24:01 -0700

    character page styping3

---
+++ main.js
@@ -0,0 +1,23 @@
+// page info pending front end team
+
+
+$(document).ready(function () {
+    var queryUrl = "http://loremricksum.com/api/?paragraphs=1&quotes=1"
+    // var queryUrl2 = "http://loremricksum.com/api/?paragraphs=1&quotes=2"
+
+    $("#mortyBtn").on("click", function(event) {
+        event.preventDefault()
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function(response) {
+            var data = response.data
+            console.log(data)
+            for (let i = 0; i < data.length; i++ ){
+                $("#mortyGroup").append(`<li class="list-group-item">${data[i]}</li>`)
+            }
+            //var quoteText = response
+            //$("#mortyGroup").html(`<li class="list-group-item">${quoteText}</li>`)
+        })
+    })
+})
